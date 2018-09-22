56b098aa82e82370759a2430821cb8aaa96cc2c3
evangile2vie <evangile2vie@gmail.com>
2018-09-22 22:27:05 +0200

    Adding new file to manage sqlite and changing laucher icon

---
+++ QuestionsReponses/ScoreCell.swift
@@ -0,0 +1,24 @@
+//
+//  ScoreCell.swift
+//  QuestionsReponses
+//
+//  Created by Eric Digbeu on 22/09/2018.
+//  Copyright © 2018 ANJC Productions. All rights reserved.
+//
+
+import UIKit
+
+class ScoreCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
